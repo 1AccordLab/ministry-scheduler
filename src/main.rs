ced7dca696620da0ca2d88d80a8c54ede0b2183a
Yu Chen <rabbit19981023@gmail.com>
2026-01-19 20:32:42 +0800

    example: hello world example with axum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+#[actix::main]
+async fn main() {
+    let addr = "0.0.0.0:3000";
+    let app = Router::new().route("/", get(index));
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+
+    println!("Server is listening on {}...", addr);
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn index() -> String {
+    String::from("Hello, world!")
 }
